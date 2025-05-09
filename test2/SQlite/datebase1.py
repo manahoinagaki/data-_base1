8124cc4f7b5e49f4ce19453e70499a8594d88442
Inagaki Manaho <manaho_inagaki0622@icloud.com>
2025-05-09 10:03:54 +0900

    a

---
+++ test2/SQlite/datebase1.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+dbname1 = 'test.db'
+conn = sqlite3.connect(dbname1,isolation_level=None)
+cursor = conn.cursor()
+
+sql = """create table if not exists users(
+    id, name, date)"""
+
+cursor.execute(sql)
+conn.commit()
+
+sql = """select name from splite_master where type='table'"""
+
+sql = """INSERT INTO test VALUES(?,?,?)"""
+
+data = [
+   (1, "Taro", 19800810),
+   (2, "Bob", 19921015),
+   (3, "Masa", 20050505),
+   (4, "Jiro", 19910510),
+   (5, "Satoshi", 19880117)
+]
+
+for t in cursor.execute(sql):
+    print(t)   
+
+corsor.executemany(sql, data)
+conn.commit()
+
+spl = """SELECT * FROM test"""
+corsor.execute(spl)
+print(corsor.fetchall())
+conn.close()
