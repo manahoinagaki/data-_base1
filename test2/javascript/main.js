aea9de225c339680533a7ecc08b1ec6955ac9178
Inagaki Manaho <manaho_inagaki0622@icloud.com>
2025-05-12 11:46:54 +0900

    a

---
+++ test2/javascript/main.js
@@ -0,0 +1,1 @@
+location.href = "file:///c%3A/Users/T24091/OneDrive%20-%20%E6%84%9B%E7%9F%A5%E5%B7%A5%E6%A5%AD%E5%A4%A7%E5%AD%A6/%E3%82%A2%E3%83%97%E3%83%AA%E9%96%8B%E7%99%BA/data-_base1/test2/templates/search.html";
