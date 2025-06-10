eaa2fab30c90d70ea77b38d6942d2e4f598c299d
Inagaki Manaho <manaho_inagaki0622@icloud.com>
2025-06-10 12:07:22 +0900

    a

--- tast3/python/sql.py
+++ tast3/python/sql.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 db = sqlite3.connect(
     'tast3.db',
@@ -16,6 +17,14 @@ sql = """
                 newevent integer,
                 student integer
         )"""
+with open('tast3.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)  # Skip header row
+    for row in reader:
+        db.execute("insert into EVENT (come, answer, tobiiri, kikenn, notcome, newevent, student) 
+                   values (?, ?, ?, ?, ?, ?, ?)", row)
+        db.commit()
+        db.close()
 
 db.execute(sql)
 db.close()
