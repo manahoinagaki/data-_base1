ff976cc33cb3bf4ee214c95329af249d27a2bbcd
Inagaki Manaho <manaho_inagaki0622@icloud.com>
2025-06-20 09:13:55 +0900

    a

---
+++ test4/python/spl.py
@@ -0,0 +1,38 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect(
+    'tast3.db',
+)
+cursor = conn.cursor()
+
+cursor.execute("""
+        create table if not exists EVENT (
+                id integer primary key autoincrement,
+                come integer,
+                answer integer,
+                tobiiri integer,
+                kikenn integer,
+                notcome integer,
+                newevent integer,
+                student integer
+        )
+""")
+
+
+with open('test.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        cursor.execute("insert into EVENT (come, answer, tobiiri, kikenn, notcome, newevent, student) values (?, ?, ?, ?, ?, ?, ?)", (row['come'], row['answer'], row['tobiiri'], row['kikenn'], row['notcome'], row['newevent'], row['student']))
+        conn.commit()
+
+
+cursor.execute("select * from EVENT")
+conn.commit()
+rows = cursor.fetchall()
+print("ROWS",rows)
+for row in rows:
+    print(row)
+
+# Removed invalid line with syntax error
+conn.close()
